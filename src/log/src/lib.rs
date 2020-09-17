52947eb6dec99d1ed77b4bf4a65772bb44c44b17
hundredeir <20334754+hundredeir@users.noreply.github.com>
2020-09-17 19:58:48 +0530

    Refactor code, and add bug fixes
    1. Make structs public
    2. Use `write_all` , and `read_exact` that will gauranteed use all of the buffer
    3. Misc

--- src/log/src/lib.rs
+++ src/log/src/lib.rs
@@ -19,7 +19,7 @@ pub enum Record {
     TypeDelete(OpCode, usize, String),
 }
 
-struct LogWriter {
+pub struct LogWriter {
     writer: File,
 }
 
@@ -35,40 +35,42 @@ impl LogWriter {
     pub fn put(&mut self, key: &str, val: &str) -> io::Result<usize> {
         let mut vec: Vec<u8> = Vec::new();
 
-        vec.push(1);
+        vec.push(OpCode::Write as u8);
         vec.append(&mut (key.len() as KeyType).to_be_bytes().to_vec());
         vec.append(&mut (val.len() as KeyType).to_be_bytes().to_vec());
         vec.append(&mut key.as_bytes().to_vec());
         vec.append(&mut val.as_bytes().to_vec());
 
-        self.writer.write(&vec)
+        self.writer.write_all(&vec)?;
+        self.writer.flush()?; //Even though writer puts all bytes in file instaneously. Still for extra surity flush is called.
+
+        Ok(vec.len())
     }
 
     pub fn delete(&mut self, key: &str) -> io::Result<usize> {
         let mut vec: Vec<u8> = Vec::new();
 
-        vec.push(0);
+        vec.push(OpCode::Delete as u8);
         vec.append(&mut (key.len() as KeyType).to_be_bytes().to_vec());
         vec.append(&mut key.as_bytes().to_vec());
 
-        self.writer.write(&vec)
+        self.writer.write_all(&vec)?;
+        self.writer.flush()?; //Even though writer puts all bytes in file instaneously. Still for extra surity flush is called.
+
+        Ok(vec.len())
     }
 
-    pub fn read<P: AsRef<Path>>(path: P) -> LogReader {
-        let f = File::open(path).unwrap();
-
-        LogReader::new(f)
-    }
 }
 
-struct LogReader {
+pub struct LogReader {
     buffer_reader: BufReader<File>,
 }
 
 impl LogReader {
     const PAGE_SIZE: usize = 512;
 
-    pub fn new(f: File) -> LogReader {
+    pub fn new<P: AsRef<Path>>(path: P) -> LogReader {
+        let f = File::open(path).unwrap();
         LogReader {
             buffer_reader: BufReader::with_capacity(Self::PAGE_SIZE, f),
         }
@@ -80,43 +82,32 @@ impl Iterator for LogReader {
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut opcode: [u8; 1] = [0; 1];
-        let mut n;
 
-        n = self.buffer_reader.read(&mut opcode[..]).unwrap();
-
-        if n == 0 {
-            return None;
-        }
+        self.buffer_reader.read_exact(&mut opcode[..]).unwrap();
 
         const KEY_SIZE: usize = mem::size_of::<KeyType>();
         let mut size_buff: [u8; KEY_SIZE] = [0; KEY_SIZE];
 
-        match opcode[0] {
-            0 => {
-                n = self.buffer_reader.read(&mut size_buff).unwrap();
+        const WRITE: u8 = OpCode::Write as u8;
+        const DELETE: u8 = OpCode::Delete as u8;
 
-                assert_ne!(n, 0, "Corrupt data");
-                assert_eq!(n, KEY_SIZE);
+        match opcode[0] {
+            WRITE => {
+                self.buffer_reader.read_exact(&mut size_buff).unwrap();
 
                 let key_length = usize::from_be_bytes(size_buff);
 
-                n = self.buffer_reader.read(&mut size_buff).unwrap();
-                if n == 0 {
-                    panic!("Corrupt data");
-                }
+                self.buffer_reader.read_exact(&mut size_buff).unwrap();
+
                 let val_length = usize::from_be_bytes(size_buff);
 
                 let mut buffer: Vec<u8> = vec![0; key_length];
-                n = self.buffer_reader.read(&mut buffer).unwrap();
-                assert_ne!(n, 0, "Corrupt data");
-                assert_eq!(n, key_length);
+                self.buffer_reader.read_exact(&mut buffer).unwrap();
 
                 let key = std::str::from_utf8(&buffer).unwrap().to_string();
 
                 buffer.resize(val_length, 0);
-                n = self.buffer_reader.read(&mut buffer).unwrap();
-                assert_ne!(n, 0, "Corrupt data");
-                assert_eq!(n, val_length);
+                self.buffer_reader.read_exact(&mut buffer).unwrap();
 
                 let val = std::str::from_utf8(&buffer).unwrap().to_string();
 
@@ -128,18 +119,13 @@ impl Iterator for LogReader {
                     val,
                 ))
             }
-            1 => {
-                n = self.buffer_reader.read(&mut size_buff).unwrap();
-
-                assert_ne!(n, 0, "Corrupt data");
-                assert_eq!(n, KEY_SIZE);
+            DELETE => {
+                self.buffer_reader.read_exact(&mut size_buff).unwrap();
 
                 let key_length = usize::from_be_bytes(size_buff);
 
                 let mut buffer: Vec<u8> = vec![0; key_length];
-                n = self.buffer_reader.read(&mut buffer).unwrap();
-                assert_ne!(n, 0, "Corrupt data");
-                assert_eq!(n, key_length);
+                self.buffer_reader.read_exact(&mut buffer).unwrap();
 
                 let key = std::str::from_utf8(&buffer).unwrap().to_string();
 
