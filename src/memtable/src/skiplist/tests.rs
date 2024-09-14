6f47d296d2f1b4a2a9e281c1f8b2a0859dba2bb7
bruisedsamurai <20334754+bruisedsamurai@users.noreply.github.com>
2024-09-14 22:34:36 +0530

    Refactor skiplist testcases to include hazard pointers

--- src/memtable/src/skiplist/tests.rs
+++ src/memtable/src/skiplist/tests.rs
@@ -1,75 +1,164 @@
 ﻿#[cfg(test)]
 mod tests {
+    use super::super::HazarPointerRecord;
     use super::*;
     use crate::skiplist::SkipList;
-    use rand::distributions::{Alphanumeric, DistString};
-    use rand::Rng;
+    use std::sync::atomic::{AtomicPtr, AtomicU32, Ordering};
+    use std::sync::Arc;
+    use std::thread;
 
     #[test]
     fn test_skiplist() {
         let mut skiplist = SkipList::new();
-        skiplist.add(0, "a");
-        skiplist.add(1, "b");
-        skiplist.add(2, "c");
-        skiplist.add(3, "d");
-        skiplist.add(4, "e");
-        skiplist.add(5, "f");
-        skiplist.add(6, "g");
-        skiplist.add(7, "h");
-        skiplist.add(8, "i");
-        skiplist.add(9, "j");
+        let mut head = Arc::new(AtomicPtr::new(std::ptr::null_mut()));
+        let mut total_hptr_count = Arc::new(AtomicU32::new(0));
+        let hp_record = HazarPointerRecord::allocate_hp_record(
+            Arc::clone(&head),
+            Arc::clone(&total_hptr_count),
+            5,
+        );
+        skiplist.add(0, "a", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(1, "b", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(2, "c", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(3, "d", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(4, "e", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(5, "f", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(6, "g", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(7, "h", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(8, "i", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(9, "j", hp_record, head.load(Ordering::SeqCst));
 
-        let result = skiplist.find(0);
+        let result = skiplist.find(0, hp_record, head.load(Ordering::SeqCst));
         assert_eq!(result.success, true);
-        let result = skiplist.find(1);
+        let result = skiplist.find(1, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(2);
+        let result = skiplist.find(2, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(3);
+        let result = skiplist.find(3, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(4);
+        let result = skiplist.find(4, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(5);
+        let result = skiplist.find(5, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(6);
+        let result = skiplist.find(6, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(7);
+        let result = skiplist.find(7, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(8);
+        let result = skiplist.find(8, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
-        let result = skiplist.find(9);
+        let result = skiplist.find(9, hp_record, head.load(Ordering::SeqCst));
         assert!(result.success);
     }
 
     #[test]
     fn test_skiplist_remove() {
         let mut skiplist = SkipList::new();
-        skiplist.add(0, "a");
-        skiplist.add(1, "b");
-        skiplist.add(2, "c");
-        skiplist.add(3, "d");
-        skiplist.add(4, "e");
-        skiplist.add(5, "f");
-        skiplist.add(6, "g");
-        skiplist.add(7, "h");
-        skiplist.add(8, "i");
-        skiplist.add(9, "j");
 
-        let success = skiplist.remove(0);
+        let mut head = Arc::new(AtomicPtr::new(std::ptr::null_mut()));
+        let mut total_hptr_count = Arc::new(AtomicU32::new(0));
+        let hp_record = HazarPointerRecord::allocate_hp_record(
+            Arc::clone(&head),
+            Arc::clone(&total_hptr_count),
+            5,
+        );
+
+        skiplist.add(0, "a", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(1, "b", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(2, "c", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(3, "d", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(4, "e", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(5, "f", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(6, "g", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(7, "h", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(8, "i", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(9, "j", hp_record, head.load(Ordering::SeqCst));
+
+        let success = skiplist.remove(0, hp_record, head.load(Ordering::SeqCst));
         assert!(success);
-        let result = skiplist.find(0);
+        let result = skiplist.find(0, hp_record, head.load(Ordering::SeqCst));
         assert!(!result.success);
-        let success = skiplist.remove(1);
+        let success = skiplist.remove(1, hp_record, head.load(Ordering::SeqCst));
         assert!(success);
-        let result = skiplist.find(1);
+        let result = skiplist.find(1, hp_record, head.load(Ordering::SeqCst));
         assert!(!result.success);
-        let success = skiplist.remove(2);
+        let success = skiplist.remove(2, hp_record, head.load(Ordering::SeqCst));
         assert!(success);
-        let result = skiplist.find(2);
+        let result = skiplist.find(2, hp_record, head.load(Ordering::SeqCst));
         assert!(!result.success);
-        let success = skiplist.remove(3);
+        let success = skiplist.remove(3, hp_record, head.load(Ordering::SeqCst));
         assert!(success);
-        let result = skiplist.find(3);
+        let result = skiplist.find(3, hp_record, head.load(Ordering::SeqCst));
         assert!(!result.success);
+    }
+
+    #[test]
+    fn test_skiplist_parallel_remove() {
+        let mut skiplist = SkipList::new();
+
+        let mut head = Arc::new(AtomicPtr::new(std::ptr::null_mut()));
+        let mut total_hptr_count = Arc::new(AtomicU32::new(0));
+        let hp_record = HazarPointerRecord::allocate_hp_record(
+            Arc::clone(&head),
+            Arc::clone(&total_hptr_count),
+            5,
+        );
+
+        skiplist.add(0, "a", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(1, "b", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(2, "c", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(3, "d", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(4, "e", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(5, "f", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(6, "g", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(7, "h", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(8, "i", hp_record, head.load(Ordering::SeqCst));
+        skiplist.add(9, "j", hp_record, head.load(Ordering::SeqCst));
+
+        thread::scope(|s| {
+            let arc_head = Arc::clone(&head);
+            let arc_total_hptr_count = Arc::clone(&total_hptr_count);
+
+            s.spawn(|| {
+                let hp_record = HazarPointerRecord::allocate_hp_record(
+                    Arc::clone(&head),
+                    Arc::clone(&total_hptr_count),
+                    5,
+                );
+                let success = skiplist.remove(0, hp_record, head.load(Ordering::SeqCst));
+                assert!(success);
+            });
+
+            let handle = s.spawn(|| {
+                let hp_record = HazarPointerRecord::allocate_hp_record(
+                    Arc::clone(&head),
+                    Arc::clone(&total_hptr_count),
+                    5,
+                );
+                let success = skiplist.remove(1, hp_record, head.load(Ordering::SeqCst));
+                assert!(success);
+            });
+
+            handle.join().unwrap();
+
+            s.spawn(|| {
+                let hp_record = HazarPointerRecord::allocate_hp_record(
+                    Arc::clone(&head),
+                    Arc::clone(&total_hptr_count),
+                    5,
+                );
+                let success = skiplist.remove(2, hp_record, head.load(Ordering::SeqCst));
+                assert!(success);
+            });
+
+            s.spawn(|| {
+                let hp_record = HazarPointerRecord::allocate_hp_record(
+                    Arc::clone(&head),
+                    Arc::clone(&total_hptr_count),
+                    5,
+                );
+                let success = skiplist.remove(3, hp_record, head.load(Ordering::SeqCst));
+                assert!(success);
+            });
+        });
     }
 }
